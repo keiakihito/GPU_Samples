b4f6a203093ef3c59dfd5258613d74bcb83d9377
keiakihito <kkatsumi.git@gmail.com>
2024-03-02 21:10:57 -0800

    Modifiying basicSgemm_h function

--- HW1/HostFunctinos/main.c
+++ HW1/HostFunctinos/main.c
@@ -14,6 +14,12 @@ void fillUpArray(int numOfRow, int numOfClm, float *ptr_h)
     }
 } // end of fillUpArray
 
+//Input:
+//int numOfRow, number of row a matrix
+//int numOfClm, number of column a matrix
+//const float *ptr_h, dynamic array inital index address
+//Process: the function prints the value of array
+//Output: void
 void printArray(int numOfRow, int numOfClm, const float *ptr_h)
 {
     for (int rWkr = 0; rWkr < numOfRow; rWkr++) {
@@ -24,19 +30,52 @@ void printArray(int numOfRow, int numOfClm, const float *ptr_h)
     }// end of outer loop
 } // end of printArray
 
+//Input:
+//int m, number of row matrixA
+//int k, number of column matrixA, and number of row matrixB
+//int n, number of column matrixB
+//Process matrix multiplicatoin C = AB
+//Output void.
 void basicSgemm_h(int m, int k, int n, const float *A_h, const float *B_h, float* C_h)
 {
+    float sum = 0.0;
+    for (int rWkr = 0; rWkr < m; rWkr++) {
+        for(int cWkr = 0; cWkr < n; cWkr++) {
+            for(int kWkr = 0; kWkr < k; kWkr++) {
+                // printf("\nA_h[%d]", rWkr * k + kWkr);
+                // printf("\nB_h[%d]", kWkr * n +cWkr);
+                sum += A_h[rWkr * k + kWkr] * B_h[kWkr * n +cWkr];
+            } // end of inner loop 2
+            // printf("\nrWkr: %d, cWkr: %d", rWkr, cWkr);
+            // printf("\nSum: %f", sum);
+            // printf("\nC_h[%d]", rWkr*k + cWkr);
+            C_h[rWkr*n+ cWkr] = sum;
+            sum = 0.0;
+        } // end of inner loop 1
 
-}
+    }// end of outer loop
+}// end of basicSgemm_h
 
 int main(void)
 {
-    int r = 4, c = 4;
+    int m = 6, k = 1, n = 6;
 
-    float* ptr = malloc((r * c) * sizeof(float));
-    fillUpArray(r, c, ptr);
-    printArray(r, c, ptr);
+    float* ptr_A = malloc((m * k) * sizeof(float));
+    printf("\n Matrix A: \n");
+    fillUpArray(m, k, ptr_A);
+    printArray(m, k, ptr_A);
 
+    printf("\n Matrix B: \n");
+    float* ptr_B = malloc((k* n) * sizeof(float));
+    fillUpArray(k, n, ptr_B);
+    printArray(k, n, ptr_B);
+
+    float* ptr_C = malloc((m * n) * sizeof(float));
+    printf("\nMultipliation:");
+    basicSgemm_h(m,k,n, ptr_A, ptr_B, ptr_C);
+
+    printf("\nMatrix C:\n");
+    printArray(m,n,ptr_C);
 
     return 0;
 }
