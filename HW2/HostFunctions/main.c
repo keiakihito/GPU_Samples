ee806828ea2e246b2ee386ef1cf79af4bf4efd38
keiakihito <kkatsumi.git@gmail.com>
2024-03-22 21:19:57 -0700

    Updating HostFunctions for Prototype trying to tile copy from source matrix to dynamically allocate matrix inside kernel for HW2

--- HW2/HostFunctions/main.c
+++ HW2/HostFunctions/main.c
@@ -49,6 +49,8 @@ void copyTileForA(const int TILE_WIDTH, int t_wkr, const int MTX_ROW,  const int
             if (srcRow < MTX_ROW && srcCol < MTX_WIDTH) { // Check to avoid out-of-bounds access
                 mtx_s[r_wkr][c_wkr] = srcMtxPtr_h[srcRow * MTX_WIDTH + srcCol];
                 // printf("\n %f", mtx_s[r_wkr][c_wkr]);
+            }else {
+                mtx_s[r_wkr][c_wkr] = 0.0f;
             }
         } // end of inner loop for column
     } // end of outer loop for row
@@ -70,6 +72,8 @@ void copyTileForB(const int TILE_WIDTH, int t_wkr, const int MTX_ROW,  const int
             if (srcRow < MTX_ROW && srcCol < MTX_WIDTH) { // Check to avoid out-of-bounds access
                 mtx_s[r_wkr][c_wkr] = srcMtxPtr_h[srcRow * MTX_WIDTH + srcCol];
                 // printf("\n %f", mtx_s[r_wkr][c_wkr]);
+            }else {
+                mtx_s[r_wkr][c_wkr] = 0.0f;
             }
         } // end of inner loop for column
     } // end of outer loop for row
@@ -143,7 +147,7 @@ void basicSgemm_h(int m, int k, int n, const float *A_h, const float *B_h, float
 //int n, number of column matrixB
 //Process A CUDA with shared memory tile techniche
 //Output void.
-void  matrixMulKernel_static_without_boundry(int m, int k, int n, const float* A_d, const float *B_d, float* C_d)
+void  matrixMulKernel_static(int m, int k, int n, const float* A_d, const float *B_d, float* C_d)
 {
     //Suppose I have 8 by7  matrix in total 9 index
     // 3 X 3 tile format
@@ -154,11 +158,15 @@ void  matrixMulKernel_static_without_boundry(int m, int k, int n, const float* A
     float A_s[TILE_DIM][TILE_DIM];
     float B_s[TILE_DIM][TILE_DIM];
     // printf("%d", (int)floor(k / TILE_DIM));
-    int numOfTile = (int)floor(k / TILE_DIM);
+    int numOfTile = ceil(k / (float)TILE_DIM);
+    printf("%d", numOfTile);
     // int t_wkr =0;
     // printf("%d", t_wkr< numOfTile);
 
+    //
     for(int t_wkr = 0 ; t_wkr < numOfTile; t_wkr++) {
+
+        //Load data from global to shared memory
         copyTileForA(TILE_DIM, t_wkr, m, k, A_d, A_s);
         if(debug) {
             printf("\n Matrix A_s: \n");
@@ -171,22 +179,63 @@ void  matrixMulKernel_static_without_boundry(int m, int k, int n, const float* A
             printArray(TILE_DIM,TILE_DIM,B_s);
         }
 
+        float sum = 0.0f;
+        int targetRow = 1;
+        int targetClm = 1;
+        for(int wkr = 0; wkr < TILE_DIM; wkr++) {
+            sum += A_s[targetRow][wkr] * B_s[wkr][targetClm];
+        } //
+        C_d[targetRow* n + targetClm] += sum;
     } // end of loop
-
-    // //Boundry condition
-    // if(rowGlbIdx < m && clmGlbIdx < n) {
-    //     for(unsigned int wkr = 0; wkr < k; wkr++) {
-    //         sum += A_d[rowGlbIdx*k + wkr] * B_d[wkr*n+clmGlbIdx];
-    //     }
-    //     C_d[rowGlbIdx*n + clmGlbIdx] = sum;
-    // } // end of if
 
 }// end of matrixMulKernel_1thread1element
 
+void matrixMulKernel_tiled(int m, int k, int n, const float* A_d, const float *B_d, float* C_d, unsigned Adz_sz, unsigned Bdz_sz) {
+
+    bool debug = true;
+
+    float* ptr_s = malloc((Adz_sz + Bdz_sz) * sizeof(float));
+    //First index of address for the TileMatrix A in the dynamic shared memory
+    float* A_shrd = ptr_s;
+    //First index of address for the TileMatrix B in the dynamic shared memory
+    float* B_shrd = ptr_s + Adz_sz;
+
+    int const TILE_WIDTH = floor(sqrt(Adz_sz / (float)2  / 4));
+    // printf("TILE_WIDTH: %d", TILE_WIDTH);
+
+    int glbRow = 1;
+    int glbCol = 1;
+    float sum = 0.0f;
+
+    for (int ot_wkr = 0; ot_wkr<(k / (float)TILE_WIDTH); ot_wkr++) {
+        copyTileForA(TILE_WIDTH, ot_wkr, m, k, A_d, A_shrd);
+        if(debug) {
+            printf("\n Matrix A_s: \n");
+            printArray(TILE_WIDTH,TILE_WIDTH,A_shrd);
+        }
+
+        copyTileForB(TILE_WIDTH, ot_wkr, k, n, B_d, B_shrd);
+        if(debug) {
+            printf("\n Matrix B_s: \n");
+            printArray(TILE_WIDTH,TILE_WIDTH,B_shrd);
+        }
+
+        for(int in_wkr = 0; in_wkr < TILE_WIDTH; in_wkr++) {
+            sum += A_shrd[glbRow*TILE_WIDTH + in_wkr] * B_shrd[in_wkr*TILE_WIDTH + glbCol];
+        } // end of inner loop
+
+        C_d[glbRow * n + glbCol] = sum;
+
+    } // end of outer loop
+
+
+
+
+} // end of matrixMulKernel_tiled
 
 int main()
 {
-    int m = 4, k = 4, n = 4;
+    int m = 5, k = 5, n = 5;
 
     float* ptr_A = malloc((m * k) * sizeof(float));
     printf("\n Matrix A: \n");
@@ -198,9 +247,18 @@ int main()
     fillUpArray(k, n, ptr_B);
     printArray(k, n, ptr_B);
 
-    float* ptr_C = malloc((m* n) * sizeof(float));
+    float* ptr_cpu= malloc((m* n) * sizeof(float));
+    float* ptr_tile = malloc((m* n) * sizeof(float));
 
-    matrixMulKernel_static_without_boundry(m,k,n, ptr_A, ptr_B, ptr_C);
+    basicSgemm_h(m,k,n,ptr_A, ptr_B, ptr_cpu);
+    // matrixMulKernel_static(m,k,n, ptr_A, ptr_B, ptr_tile);
+    matrixMulKernel_tiled(m,k,n,ptr_A, ptr_B, ptr_tile, 32, 32);
+
+    printf("\n Matrix CPU: \n");
+    printArray(m,k, ptr_cpu);
+
+    printf("\n Matrix Tile: \n");
+    printArray(m, k, ptr_tile);
 
 } // end of main
 
